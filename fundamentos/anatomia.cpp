f6908af89da982a41654d53eeafd11b51c03e11e
Fernando Shibata <shibata.421@gmail.com>
2022-07-31 13:32:32 -0300

    First commit

---
+++ fundamentos/anatomia.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    std::cout
+        <<
+        "Ola"
+        << 
+        "Bye"
+        <<
+        std::endl;
+
+    std::cout << "end version 2";
+    
+
+    return 0;
+}
